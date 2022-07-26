9aba2118f7778007180e2b75d31ed0a25669c4b4
ha-pu <66355167+ha-pu@users.noreply.github.com>
2022-07-26 19:07:49 +0200

    Update 00_setup_packges.r

--- Code/00_setup_packges.r
+++ Code/00_setup_packges.r
@@ -1,6 +1,7 @@
 ### install required packages
 
 if (!require("bibliometrix")) install.packages("bibliometrix")
+if (!require("mice")) install.packages("mice")
 if (!require("readxl")) install.packages("readxl")
 if (!require("stargazer")) install.packages("stargazer")
 if (!require("tidytext")) install.packages("tidytext")
